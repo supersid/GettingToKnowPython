ed447cef25251ab87f8af1b5b87c70d1a6a30628
Siddharth gulati <sid441.sg@gmail.com>
2019-07-04 23:16:30 +0530

    Conversion to any base code added

--- Functions/function2.py
+++ Functions/function2.py
@@ -1,12 +1,20 @@
-import pdb
-def DecimalToOctal(decimal):
-    octal,q,r=0,0,0
-    # pdb.set_trace()
-    while(decimal!=0):
-        q = decimal//8
-        r = decimal%8
-        decimal = q
-        octal = octal*10 + r
-    print(octal)
-decimal = int(input("Enter Decimal Number:"))
-DecimalToOctal(decimal)    
+def SbToDb(ib,fb,number):
+    result, rem, i = 0, 0, 0
+
+    while number!=0:
+        rem = number%10
+        number = number//10
+        result =  result+(rem*pow(ib,i))
+        i = i+1
+    final=0
+    i = 0
+    while result!=0:
+        rem = result%fb
+        result = result//fb
+        final = final+(rem*pow(10,i))
+        i = i+1
+    print(final)
+ib = int(input("Enter Initial Base:"))
+fb = int(input("Enter final Base:"))
+number = int(input("Enter Number to be converted:"))
+SbToDb(ib,fb,number)
